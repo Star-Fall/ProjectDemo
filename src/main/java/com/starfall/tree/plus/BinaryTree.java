18fcb4acedc66dd9151db64c6130f2165e56dcbb
StarFall <Starfall121039156@gmail.com>
2019-05-23 17:31:04 +0800

    UPDATE daily 190-pc

--- src/main/java/com/starfall/tree/plus/BinaryTree.java
+++ src/main/java/com/starfall/tree/plus/BinaryTree.java
@@ -1,6 +1,8 @@
 package com.starfall.tree.plus;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author StarFall
@@ -8,12 +10,13 @@ import java.util.List;
  * @package com.starfall.tree.plus
  * @className BinaryTree
  * @date 2019/5/22 16:25
- * @description BinaryTree
+ * @description BinaryTree二叉树
  */
 public class BinaryTree<T> {
 
 	private TreeNode<T> root;
 	private static int LIST_INDEX = 0;
+	private static int LEVEL_LIST_INDEX = 1;
 
 	public BinaryTree() {
 		root = new TreeNode<>();
@@ -22,6 +25,72 @@ public class BinaryTree<T> {
 	public BinaryTree(T data) {
 		root = new TreeNode<>();
 		root.data = data;
+	}
+
+	private BinaryTree(TreeNode<T> node) {
+		this.root = node;
+	}
+
+	/**
+	 * 增加某个节点的左右子树
+	 */
+	public void todo() {
+
+	}
+
+	/**
+	 * 获取根节点
+	 * 
+	 * @return 根节点
+	 */
+	public TreeNode<T> getRoot() {
+		return this.root;
+	}
+
+	/**
+	 * 根据节点数据获取子树
+	 * 
+	 * @param t
+	 *            节点数据
+	 * @return 子树
+	 */
+	public BinaryTree<T> getBTree(T t) {
+		TreeNode<T> node = getNodeByValue(this.root, t);
+		if (node == null) {
+			return null;
+		} else {
+			return new BinaryTree<T>(node);
+		}
+	}
+
+	private TreeNode<T> getNodeByValue(TreeNode<T> node, T t) {
+		if (node == null) {
+			return null;
+		} else if (node.data == t) {
+			return node;
+		} else {
+			return getNodeByValue(node.left, t) == null ? getNodeByValue(node.right, t) : getNodeByValue(node.left, t);
+		}
+	}
+
+	/**
+	 * 获取叶子节点数目
+	 * 
+	 * @return 叶子节点数目
+	 */
+	public int getLeafNum() {
+		return getLeafNum(this.root);
+	}
+
+	private int getLeafNum(TreeNode<T> node) {
+		if (node == null) {
+			return 0;
+		} else if (node.left == null && node.right == null) {
+			return 1;
+		} else {
+			return getLeafNum(node.left) + getLeafNum(node.right);
+		}
+
 	}
 
 	/**
@@ -59,23 +128,44 @@ public class BinaryTree<T> {
 	}
 
 	/**
-	 * 增加左子树
+	 * 设置左子树
 	 *
 	 * @param tree
 	 *            左子树
 	 */
-	public void addLeftTree(BinaryTree<T> tree) {
+	public void setLeftTree(BinaryTree<T> tree) {
 		this.root.left = tree.root;
 	}
 
 	/**
-	 * 增加右子树
+	 * 设置右子树
 	 *
 	 * @param tree
 	 *            右子树
 	 */
-	public void addRightTree(BinaryTree<T> tree) {
+	public void setRightTree(BinaryTree<T> tree) {
 		this.root.right = tree.root;
+	}
+
+	/**
+	 * 层序遍历创建二叉树
+	 * 
+	 * @param list
+	 */
+	public void levelCreateTree(List<T> list) {
+		this.root = levelCreateTree(this.root, list, LEVEL_LIST_INDEX);
+	}
+
+	private TreeNode<T> levelCreateTree(TreeNode<T> node, List<T> list, int i) {
+		if (i > list.size() || list.get(i - 1) == null) {
+			node = null;
+		} else {
+			T t = list.get(i - 1);
+			node = new TreeNode<>(t);
+			node.left = levelCreateTree(node.left, list, 2 * i);
+			node.right = levelCreateTree(node.right, list, 2 * i + 1);
+		}
+		return node;
 	}
 
 	/**
@@ -154,10 +244,34 @@ public class BinaryTree<T> {
 		System.out.print(node.data + "\t");
 	}
 
+	/**
+	 * 层序遍历二叉树
+	 */
+	public void levelOrder() {
+		System.out.print("[");
+		levelOrder(this.root);
+		System.out.println("]");
+	}
+
+	private void levelOrder(TreeNode<T> node) {
+		Queue<TreeNode<T>> q = new LinkedList<TreeNode<T>>();
+		q.offer(root);
+		while (!q.isEmpty()) {
+			TreeNode<T> temp = q.poll();
+			System.out.print(temp.data + "\t");
+			if (temp.left != null) {
+				q.offer(temp.left);
+			}
+			if (temp.right != null) {
+				q.offer(temp.right);
+			}
+		}
+	}
+
 	public static class TreeNode<T> {
-		TreeNode<T> left;
-		TreeNode<T> right;
-		T data;
+		private TreeNode<T> left;
+		private TreeNode<T> right;
+		private T data;
 
 		public TreeNode() {
 
@@ -165,6 +279,18 @@ public class BinaryTree<T> {
 
 		public TreeNode(T data) {
 			this.data = data;
+		}
+
+		public TreeNode<T> getLeft() {
+			return left;
+		}
+
+		public TreeNode<T> getRight() {
+			return right;
+		}
+
+		public T getData() {
+			return data;
 		}
 	}
 }
--- src/test/java/com/starfall/tree/plus/BinaryTreeTest.java
+++ src/test/java/com/starfall/tree/plus/BinaryTreeTest.java
@@ -15,35 +15,88 @@ import java.util.List;
  */
 public class BinaryTreeTest {
 
-    @Test
-    public void test1() {
-        List<String> list = Arrays.asList("A", "B", "D", null, null, "F", "E", null, null, null, "C", "G", null, "H", null, null, "I", null, null);
-        BinaryTree<String> bt = new BinaryTree<>();
-        bt.preCreateTree(list);
-        bt.preOrder();
-        bt.midOrder();
-        bt.backOrder();
-        System.out.println(bt.getTreeDepth());
-        System.out.println(bt.getNodeNum());
-    }
+	/**
+	 * 添加子树创建二叉树
+	 */
+	@Test
+	public void test01() {
+		// 层序遍历：ABCDEFG
+		// 根节点
+		BinaryTree<String> btree = new BinaryTree<>("A");
+		// 根节点左右子树
+		BinaryTree<String> bt1, bt2;
+		bt1 = new BinaryTree<>("B");
+		btree.setLeftTree(bt1);
+		bt2 = new BinaryTree<>("C");
+		btree.setRightTree(bt2);
+		// B左右子树
+		BinaryTree<String> bt11, bt12;
+		bt11 = new BinaryTree<>("D");
+		bt1.setLeftTree(bt11);
+		bt12 = new BinaryTree<>("E");
+		bt1.setRightTree(bt12);
+		// C左右子树
+		BinaryTree<String> bt21, bt22;
+		bt21 = new BinaryTree<>("F");
+		bt2.setLeftTree(bt21);
+		bt22 = new BinaryTree<>("G");
+		bt2.setRightTree(bt22);
+		// 遍历
+		btree.preOrder();
+		btree.midOrder();
+		btree.backOrder();
+		btree.levelOrder();
+	}
 
-    @Test
-    public void test02() {
-        BinaryTree<String> btree = new BinaryTree<>("A");
-        BinaryTree<String> bt1, bt2, bt3, bt4;
-        bt1 = new BinaryTree<>("B");
-        btree.addLeftTree(bt1);
-        bt2 = new BinaryTree<>("D");
-        bt1.addLeftTree(bt2);
+	/**
+	 * 前序遍历创建二叉树
+	 */
+	@Test
+	public void test02() {
+		// 补全空节点
+		List<String> list = Arrays.asList("A", "B", "D", null, null, "F", "E", null, null, null, "C", "G", null, "H",
+				null, null, "I", null, null);
+		BinaryTree<String> bt = new BinaryTree<>();
+		bt.preCreateTree(list);
+		bt.preOrder();
+		bt.midOrder();
+		bt.backOrder();
+		bt.levelOrder();
+	}
 
-        bt3 = new BinaryTree<>("C");
-        btree.addRightTree(bt3);
-        bt4 = new BinaryTree<>("E");
-        bt3.addLeftTree(bt4);
-        bt4 = new BinaryTree<>("F");
-        bt3.addRightTree(bt4);
-        btree.preOrder();
-        btree.midOrder();
-        btree.backOrder();
-    }
+	/**
+	 * 层序遍历创建二叉树
+	 */
+	@Test
+	public void test03() {
+		// 补全空节点，转为完全二叉树
+		List<String> list = Arrays.asList("A", "B", "C", "D", "F", "G", "I", null, null, "E", null, null, "H");
+		BinaryTree<String> bt = new BinaryTree<>();
+		bt.levelCreateTree(list);
+		bt.preOrder();
+		bt.midOrder();
+		bt.backOrder();
+		bt.levelOrder();
+	}
+
+	/**
+	 * 二叉树其他用法
+	 */
+	@Test
+	public void test04() {
+		List<String> list = Arrays.asList("A", "B", "D", null, null, "F", "E", null, null, null, "C", "G", null, "H",
+				null, null, "I", null, null);
+		BinaryTree<String> bt = new BinaryTree<>();
+		bt.preCreateTree(list);
+		System.out.println(bt.getNodeNum());
+		System.out.println(bt.getTreeDepth());
+		System.out.println(bt.getLeafNum());
+		System.out.println("************");
+		BinaryTree<String> tree = bt.getBTree("F");
+		System.out.println(tree.getNodeNum());
+		System.out.println(tree.getTreeDepth());
+		System.out.println(tree.getLeafNum());
+		System.out.println(tree.getRoot().getData());
+		tree.levelOrder();
+	}
 }
