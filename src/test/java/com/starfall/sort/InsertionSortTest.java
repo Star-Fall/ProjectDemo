98a759605b7d74e2cb03243e494d4e818f02f8fc
StarFall <Starfall121039156@gmail.com>
2019-06-03 18:07:39 +0800

    UPDATE daily 190-pc

--- src/test/java/com/starfall/sort/InsertionSortTest.java
+++ src/test/java/com/starfall/sort/InsertionSortTest.java
@@ -17,9 +17,14 @@ public class InsertionSortTest {
 	@Test
 	public void test01() {
 		InsertionSort sort = new InsertionSort();
-		int[] arr = { 3,2,4,7,6,8 };
+		int[] arr = { 3, 2, 4, 7, 6, 8 };
 		sort.straightInsertionSort(arr, arr.length);
 		System.out.println(Arrays.toString(arr));
+	}
+
+	@Test
+	public void test02() {
+		System.out.println("1633147347".hashCode());
 	}
 
 }
