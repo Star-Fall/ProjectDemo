cf4f2306a6fcdec1438474a804c30bb4cf4cda3c
StarFall <Starfall121039156@gmail.com>
2019-06-13 17:27:51 +0800

    UPDATE daily 190-pc

---
+++ src/main/java/com/starfall/thread/demo02/Demo02.java
@@ -0,0 +1,19 @@
+package com.starfall.thread.demo02;
+
+import org.junit.Test;
+
+/**
+ * @author StarFall
+ * @project ProjectDemo
+ * @package com.starfall.thread.demo02
+ * @className Demo02
+ * @date 2019/6/13 10:40
+ * @description todo
+ */
+public class Demo02 {
+
+	@Test
+	public void test01() {
+
+	}
+}
---
+++ src/main/java/com/starfall/thread/demo02/Thread01.java
@@ -0,0 +1,17 @@
+package com.starfall.thread.demo02;
+
+/**
+ * @author StarFall
+ * @project ProjectDemo
+ * @package com.starfall.thread.demo02
+ * @className Thread01
+ * @date 2019/6/13 10:40
+ * @description synes
+ */
+public class Thread01 extends Thread {
+
+	@Override
+	public void run() {
+
+	}
+}
