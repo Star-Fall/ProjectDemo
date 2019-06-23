aad4bb0b4643a31b41b6b680246d6fd8b58fbe82
StarFall <Starfall121039156@gmail.com>
2019-06-23 23:38:10 +0800

    UPDATE daily DESKTOP

---
+++ src/main/java/com/starfall/thread/demo06/Demo06.java
@@ -0,0 +1,16 @@
+package com.starfall.thread.demo06;
+
+/**
+ * @author StarFall
+ * @project ProjectDemo
+ * @package com.starfall.thread.demo06
+ * @className Demo06
+ * @date 2019/6/23 18:05
+ * @description Demo06
+ */
+public class Demo06 {
+
+    public static void main(String[] args) {
+        Demo06 demo06 = new Demo06();
+    }
+}
