88a355b77bda9fec4963a9c013ba30e0387cf1e8
StarFall <Starfall121039156@gmail.com>
2019-07-03 23:51:26 +0800

    UPDATE daily DESKTOP

---
+++ src/main/java/com/starfall/thread/demo10/Demo10.java
@@ -0,0 +1,23 @@
+package com.starfall.thread.demo10;
+
+/**
+ * @author StarFall
+ * @project ProjectDemo
+ * @package com.starfall.thread.demo10
+ * @className Demo10
+ * @date 2019/7/3 23:21
+ * @description Demo10
+ */
+public class Demo10 {
+
+	public static void main(String[] args) {
+		Integer a = 1;
+		Integer b = 1;
+		System.out.println(a == b);
+
+		Integer c = 128;
+		Integer d = 128;
+		System.out.println(c == d);
+
+	}
+}
