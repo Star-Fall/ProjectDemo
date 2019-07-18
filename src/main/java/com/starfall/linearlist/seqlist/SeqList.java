cc02d3775d58af9ef98343bb910169e0d1ff9012
StarFall <Starfall121039156@gmail.com>
2019-07-18 23:28:27 +0800

    UPDATE daily DESKTOP

---
+++ src/main/java/com/starfall/linearlist/seqlist/SeqList.java
@@ -0,0 +1,19 @@
+package com.starfall.linearlist.seqlist;
+
+/**
+ * @author StarFall
+ * @project ProjectDemo
+ * @package com.starfall.linearlist.seqlist
+ * @className SeqList
+ * @date 2019/7/18 23:19
+ * @description SeqList 顺序表
+ */
+public class SeqList<T> {
+
+	private static final int MAXSIZE = 100;
+	private T data[];
+
+	public SeqList() {
+		//data = new T[MAXSIZE];
+	}
+}
