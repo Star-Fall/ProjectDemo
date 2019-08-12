b4aa85fc56601bf1e8865ce02fca6f00c8cfa782
StarFall <Starfall121039156@gmail.com>
2019-08-12 17:57:51 +0800

    UPDATE daily 190-pc

--- src/main/java/com/starfall/sort/BubbleSort.java
+++ src/main/java/com/starfall/sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package com.starfall.sort;
 
+import java.util.Arrays;
+
 /**
  * @author StarFall
  * @project ProjectDemo
@@ -8,9 +10,62 @@ package com.starfall.sort;
  * @date 2019/5/29 22:16
  * @description BubbleSort冒泡排序
  */
-public class BubbleSort<T> {
+public class BubbleSort {
 
-    public void bubbleSort(){
+	/**
+	 * 冒泡排序
+	 * 
+	 * @param arr
+	 *            int数组
+	 * @param length
+	 *            数组长度
+	 */
+	public static void bubbleSort(int arr[], int length) {
+		if (arr == null || length <= 1) {
+			return;
+		}
+		for (int i = 0; i < length - 1; i++) {
+			for (int j = 0; j < length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
 
-    }
+	/**
+	 * 冒泡排序（优化）
+	 * 
+	 * @param arr
+	 *            int数组
+	 * @param length
+	 *            数组长度
+	 */
+	public static void bubbleSortPlus(int arr[], int length) {
+		if (arr == null || length <= 1) {
+			return;
+		}
+		for (int i = 0; i < length - 1; i++) {
+			boolean flag = false;
+			for (int j = 0; j < length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					flag = true;
+				}
+			}
+			if (!false) {
+				break;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 2, 9, 7, 6, 8, 5, 4 };
+		bubbleSort(arr, arr.length);
+		System.out.println(Arrays.toString(arr));
+	}
 }
