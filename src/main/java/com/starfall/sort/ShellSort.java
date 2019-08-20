14b99f416ce7f964bd68d535a551b10faeac401e
StarFall <Starfall121039156@gmail.com>
2019-08-20 17:28:04 +0800

    UPDATE daily 190-pc

--- src/main/java/com/starfall/sort/ShellSort.java
+++ src/main/java/com/starfall/sort/ShellSort.java
@@ -39,9 +39,10 @@ public class ShellSort {
 					}
 					// 将待插入元素temp插入到无需后移的位置
 					array[j + gap] = temp;
+					System.out.println("\t排序" + (k + 1) + "小组：\t" + Arrays.toString(array));
 				}
 			}
-			System.out.println("第" + gap + "步：\t" + Arrays.toString(array));
+			System.out.println("步长" + gap + "：\t" + Arrays.toString(array));
 			// 每个分组排序完成，缩小步长
 			gap = gap / 2;
 		}
@@ -49,7 +50,7 @@ public class ShellSort {
 
 	/**
 	 * 希尔排序优化
-	 * 
+	 *
 	 * @param array
 	 *            int 数组
 	 * @param length
@@ -58,22 +59,23 @@ public class ShellSort {
 	public static void shellSortPlus(int array[], int length) {
 		int i = 0, j = 0, temp = 0, gap = length / 2;
 		while (gap >= 1) {
+			// 省却之前遍历每个分组，将分组合并遍历，根据下标穿插比较。
 			for (i = gap; i < length; i++) {
+				// 待插入元素
 				temp = array[i];
+				// 内层循环的j依次递减gap，保证了每次插入排序范围在各个小组中。
 				for (j = i - gap; j >= 0 && temp < array[j]; j = j - gap) {
 					array[j + gap] = array[j];
 				}
 				array[j + gap] = temp;
-				System.out.println("\t第" + i + "趟：\t" + Arrays.toString(array));
 			}
-			System.out.println("第" + gap + "步：\t" + Arrays.toString(array));
+			System.out.println("步长" + gap + "：\t" + Arrays.toString(array));
 			gap = gap / 2;
 		}
 	}
 
 	public static void main(String[] args) {
-		// int array[] = { 49, 38, 65, 97, 26, 13, 27, 77, 55, 4 };
-		int array[] = { 45, 23, 19, 16, 11, 9, 2, 1 };
+		int array[] = { 49, 38, 65, 97, 26, 13, 27, 77, 55, 4 };
 		System.out.println("排序前：\t" + Arrays.toString(array));
 		shellSort(array, array.length);
 		System.out.println("排序后：\t" + Arrays.toString(array));
