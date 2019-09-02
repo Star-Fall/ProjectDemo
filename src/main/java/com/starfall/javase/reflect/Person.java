08ea15365dc35205027c6bcc5ee2853a3a595030
StarFall <Starfall121039156@gmail.com>
2019-09-02 18:01:06 +0800

    UPDATE daily 190-pc

--- src/main/java/com/starfall/javase/reflect/Person.java
+++ src/main/java/com/starfall/javase/reflect/Person.java
@@ -18,14 +18,48 @@ public class Person {
 	private Date birthday;
 	private static String STR = "STR";
 
+	public String field;
+
 	public Person() {
+		System.out.println("默认无参构造方法");
+	}
+
+	public Person(String name) {
+		System.out.println("有参构造方法：String name");
+		this.name = name;
+	}
+
+	public Person(Integer code, String name) {
+		System.out.println("有参构造方法：Integer code, String name");
+		this.code = code;
+		this.name = name;
 	}
 
 	public Person(int age, Integer code, String name, Date birthday) {
+		System.out.println("有参构造方法：int age, Integer code, String name, Date birthday");
 		this.age = age;
 		this.code = code;
 		this.name = name;
 		this.birthday = birthday;
+	}
+
+	public Person(int age, Integer code, String name, Date birthday, String field) {
+		System.out.println("有参构造方法：int age, Integer code, String name, Date birthday, String field");
+		this.age = age;
+		this.code = code;
+		this.name = name;
+		this.birthday = birthday;
+		this.field = field;
+	}
+
+	protected Person(int age) {
+		System.out.println("受保护有参构造方法：int age");
+		this.age = age;
+	}
+
+	private Person(Integer code) {
+		System.out.println("私有有参构造方法：Integer code");
+		this.code = code;
 	}
 
 	public int getAge() {
@@ -68,8 +102,22 @@ public class Person {
 		Person.STR = STR;
 	}
 
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
 	@Override
 	public String toString() {
-		return "Person{" + "age=" + age + ", code=" + code + ", name='" + name + '\'' + ", birthday=" + birthday + '}';
+		return "Person{" +
+				"age=" + age +
+				", code=" + code +
+				", name='" + name + '\'' +
+				", birthday=" + birthday +
+				", field='" + field + '\'' +
+				'}';
 	}
 }
--- src/main/java/com/starfall/javase/reflect/TestClient.java
+++ src/main/java/com/starfall/javase/reflect/TestClient.java
@@ -1,6 +1,6 @@
 package com.starfall.javase.reflect;
 
-import java.lang.reflect.Constructor;
+import java.lang.reflect.*;
 
 /**
  * @author StarFall
@@ -8,7 +8,7 @@ import java.lang.reflect.Constructor;
  * @package com.starfall.javase.reflect
  * @className TestClient
  * @date 2019/8/30 14:28
- * @description todo
+ * @description TestClient 测试
  */
 public class TestClient {
 
@@ -16,18 +16,126 @@ public class TestClient {
 		try {
 			// 获取Class对象
 			Class class1 = Class.forName("com.starfall.javase.reflect.Person");
-			Person person = (Person) class1.newInstance();
+			// 使用反射创建实例对象
+			// 使用Class对象
+			Person p1 = (Person) class1.newInstance();
+			// 使用构造器对象
+			Constructor constructor = class1.getConstructor();
+			Person p2 = (Person) constructor.newInstance();
+		} catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 获取反射构造器Constructor
+	 */
+	public void test2() {
+		try {
+			Class class1 = Class.forName("com.starfall.javase.reflect.Person");
+			// 获取所有公有构造方法
+			System.out.println("************获取所有公有构造方法************");
 			Constructor[] constructors = class1.getConstructors();
 			for (Constructor constructor : constructors) {
-				System.out.println(constructor.toString());
+				System.out.println(constructor);
 			}
-		} catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+			System.out.println("************根据参数获取构造方法(public)************");
+			Constructor constructor = class1.getConstructor(String.class);
+			System.out.println(constructor);
+			// 获取所有构造方法
+			System.out.println("************获取所有构造方法************");
+			Constructor[] declaredConstructors = class1.getDeclaredConstructors();
+			for (Constructor declaredConstructor : declaredConstructors) {
+				System.out.println(declaredConstructor);
+			}
+			System.out.println("************根据参数获取构造方法(private)************");
+			Constructor declaredConstructor = class1.getDeclaredConstructor(Integer.class);
+			System.out.println(declaredConstructor);
+		} catch (ClassNotFoundException | NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Constructor常用方法
+	 */
+	public void test2_1() {
+		try {
+			Class class1 = Class.forName("com.starfall.javase.reflect.Person");
+			// 私有构造： Constructor constructor = class1.getDeclaredConstructor(Integer.class);
+			Constructor constructor = class1.getConstructor(Integer.class, String.class);
+			System.out.println("修饰符：" + Modifier.toString(constructor.getModifiers()));
+			System.out.println("构造器名称：" + constructor.getName());
+			System.out.println("构造器参数类型列表：");
+			Class[] parameterTypes = constructor.getParameterTypes();
+			for (Class parameterType : parameterTypes) {
+				System.out.println(parameterType);
+			}
+			System.out.println("构造器参数列表：");
+			Parameter[] parameters = constructor.getParameters();
+			for (Parameter parameter : parameters) {
+				System.out.println(parameter);
+			}
+			System.out.println("忽略掉访问修饰符：" + constructor.isAccessible());
+			System.out.println("构造器生成实例对象");
+			Object o = constructor.newInstance(1, "A");
+		} catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 反射成员变量Field
+	 */
+	public void test3() {
+		try {
+			Class class1 = Class.forName("com.starfall.javase.reflect.Person");
+			System.out.println("************获取所有公有成员变量************");
+			Field[] fields = class1.getFields();
+			for (Field field : fields) {
+				System.out.println(field);
+			}
+			System.out.println("************根据成员变量名称获取成员变量************");
+			Field age = class1.getDeclaredField("age");
+			System.out.println(age);
+			System.out.println("************获取所有成员变量************");
+			Field[] declaredFields = class1.getDeclaredFields();
+			for (Field declaredField : declaredFields) {
+				System.out.println(declaredField);
+			}
+		} catch (ClassNotFoundException | NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Field常用方法
+	 */
+	public void test3_1() {
+		try {
+			Class class1 = Class.forName("com.starfall.javase.reflect.Person");
+			Field name = class1.getDeclaredField("name");
+			System.out.println("修饰符：" + Modifier.toString(name.getModifiers()));
+			System.out.println("变量名称：" + name.getName());
+			System.out.println("变量类型：" + name.getType());
+
+			// 为实例对象设置字段值
+			Person person = (Person) class1.newInstance();
+			System.out.println("设置私有变量可以访问，并赋值：");
+			// 私有变量设置允许访问
+			name.setAccessible(true);
+			name.set(person, "personName");
+			System.out.println("获取修改后的属性值：" + name.get(person));
+			System.out.println(person);
+		} catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException | InstantiationException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static void main(String[] args) {
 		TestClient test = new TestClient();
-		test.test1();
+		test.test3_1();
 	}
 }
