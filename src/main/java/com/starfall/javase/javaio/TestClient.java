666af94474a2fd41029a2240473b20638a8670d2
StarFall <Starfall121039156@gmail.com>
2019-09-23 17:55:18 +0800

    UPDATE daily 190-pc

---
+++ src/main/java/com/starfall/javase/javaio/TestClient.java
@@ -0,0 +1,26 @@
+package com.starfall.javase.javaio;
+
+import java.io.File;
+
+/**
+ * @author StarFall
+ * @project ProjectDemo
+ * @package com.starfall.javase.javaio
+ * @className TestClient
+ * @date 2019/9/23 17:05
+ * @description todo
+ */
+public class TestClient {
+
+	/**
+	 * File 文件操作
+	 */
+	public void test1() {
+		File file = new File("");
+	}
+
+	public static void main(String[] args) {
+		TestClient testClient = new TestClient();
+		testClient.test1();
+	}
+}
